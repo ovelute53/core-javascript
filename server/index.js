7875c10cda89823dcd333b761100f5ef90be4be3
ovelute53 <ovelute53@naver.com>
2022-12-28 11:33:35 +0900

    globalThis

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-const liveServer = require("live-server");
+// const liveServer = require("live-server");
 
-const params = {
-  host: "localhost",
-  port: 3000,
-  open: false,
-  root: "./client",
-};
+// const params = {
+//   host: "localhost",
+//   port: 3000,
+//   open: false,
+//   root: "./client",
+// };
 
-liveServer.start(params);
+// liveServer.start(params);
