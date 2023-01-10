9f6a528cc1b6eb68680a4327bec762a49b42bcc3
ovelute53 <ovelute53@naver.com>
2023-01-10 16:22:01 +0900

    getNode lib modified

--- client/lib/dom/getNode.js
+++ client/lib/dom/getNode.js
@@ -1,14 +1,16 @@
 function getNode(node) {
   if (typeof node !== "string") {
-    throw new Error("getNode의 함수 인자는 문자 타입이어야 합니다.");
+    throw new Error("getNode 함수의 인자는 문자 타입 이여야 합니다.");
   }
+
+  // if(!isString(node)) typeError('에러가 발생했습니다.');
 
   return document.querySelector(node);
 }
 
 function getNodes(node) {
   if (typeof node !== "string") {
-    throw new Error("getNode의 함수 인자는 문자 타입이어야 합니다.");
+    throw new Error("getNode 함수의 인자는 문자 타입 이여야 합니다.");
   }
 
   return document.querySelectorAll(node);
