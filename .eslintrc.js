32602daba4826ef6a33d1128b4ae5e1c6bbfea41
ovelute53 <ovelute53@naver.com>
2023-01-13 11:54:19 +0900

    eslint warning 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  "eslint no-undef": "warn",
+  "eslint no-used-vars": "off",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
