33bc29b0ba67ee1374f917728931f53326dddc59
ovelute53 <ovelute53@naver.com>
2023-01-16 11:20:44 +0900

    주사위 굴리기 토글

--- client/main.js
+++ client/main.js
@@ -1,1 +1,21 @@
-import { dice } from "./lib/index.js";
+import { diceAnimation, getNode } from "./lib/index.js";
+
+const rollingDiceButton = getNode(".buttonGroup >button:nth-child(1)");
+
+// IIFE
+const handlerRollingDice = (() => {
+  let isRolling = false;
+  let stopAniamation;
+
+  return () => {
+    if (!isRolling) {
+      stopAniamation = setInterval(diceAnimation, 200);
+    } else {
+      clearInterval(stopAniamation);
+    }
+
+    isRolling = !isRolling;
+  };
+})();
+
+rollingDiceButton.addEventListener("click", handlerRollingDice);
